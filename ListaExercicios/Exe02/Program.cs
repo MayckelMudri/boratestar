77c987319de00b1fd94e37dd2eefde55aeaacb00
MayckelMudri <163066393+MayckelMudri@users.noreply.github.com>
2024-03-11 20:25:46 -0300

    Add files via upload

---
+++ ListaExercicios/Exe02/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Exe02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] vetor = new int[100];
+        Random random = new Random();
+        //Preenchendo o vetor com valores aleatórios
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = random.Next(10000);
+        }
+        //Imprimindo o vetor desordenado
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+        Console.WriteLine("\n");
+
+        //Ordenar o vetor
+        bool troca;
+        do
+        {
+
+            troca = false;
+            for (int i = 0; i < vetor.Length - 1; i++)
+            {
+                if (vetor[i] > vetor[i + 1])
+                {
+                    int aux = vetor[i];
+                    vetor[i] = vetor[i + 1];
+                    vetor[i + 1] = aux;
+                    troca = true;
+                }
+            }
+        } while (troca);
+
+
+
+        //Imprimindo o vetor ordenado
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+    }
+}
